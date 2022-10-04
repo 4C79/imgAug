4ac68acbcb6c17dd25d0ad16179b80addfce18e3
company_vx <1228754216@qq.com>
2022-10-04 17:35:29 +0800

    Changes

--- tool/methods.py
+++ tool/methods.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'main.py'
+# Form implementation generated from reading ui file 'methods.py'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
